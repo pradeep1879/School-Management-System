fd00d6b93146a519a1c0574e1d12b18b8c91f12c
Pradeep Kumar <pk6949137@gmail.com>
2026-03-09 00:47:02 +0530

    added frontend url

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,7 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "school-management-system-h2zj.vercel.app",
     credentials:true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"]
