f1a13320f4529258050ad5335dc7ff709cbb480e
Pradeep Kumar <pk6949137@gmail.com>
2026-03-12 22:39:00 +0530

    testing ci cd

--- client/src/features/admin/pages/Dashboard.tsx
+++ client/src/features/admin/pages/Dashboard.tsx
@@ -6,14 +6,11 @@ import AdminAnalystics from "../components/AdminAnalystics";
 
 const AdminDashboard = () => {
 
-
-
-
   return (
     <div className="space-y-10">
       {/*  HEADER  */}
       <div>
-        <h1 className="text-3xl font-bold tracking-tight">Admin Dashboard</h1>
+        <h1 className="text-3xl font-bold tracking-tight">Admin</h1>
         <p className="text-muted-foreground mt-1">
           Real-time overview of school performance
         </p>
